44839cd6cf665859d27e8bdaf7fd57b33b40b0b8
RafaelZanette <r.zanette91@gmail.com>
2021-10-05 19:00:01 +0000

    list.jsp

--- src/main/java/application/controllers/LivroCoontroller.java
+++ src/main/java/application/controllers/LivroCoontroller.java
@@ -1,10 +1,25 @@
 package application.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
+@RequestMapping("/livro")
 public class LivroCoontroller {
+    @Autowired
+    private LivroRepository livrosRepo;
+    @RequestMapping("/list")
+    public String list(Model model){
+        model.addAttribute("livros", livrosRepo());
+        return"list.jsp";
+
+
+
+    }
     
 
 
