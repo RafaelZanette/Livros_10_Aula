8d8c9a2467347512d541915b870f7c6697614c78
RafaelZanette <r.zanette91@gmail.com>
2021-10-05 19:07:55 +0000

    Acertarmos erros

--- src/main/java/application/controllers/LivroController.java
+++ src/main/java/application/controllers/LivroController.java
@@ -6,22 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import application.respositories.LivroRepository;
 
 @Controller
 @RequestMapping("/livro")
-public class LivroCoontroller {
+public class LivroController {
     @Autowired
     private LivroRepository livrosRepo;
     @RequestMapping("/list")
     public String list(Model model){
-        model.addAttribute("livros", livrosRepo());
-        return"list.jsp";
-
-
+        model.addAttribute("livros", livrosRepo.findAll());
+        return "list.jsp";
 
     }
-    
-
-
-    
+       
 }
